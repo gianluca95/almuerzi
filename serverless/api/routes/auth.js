887369fc64076e835d044287d30bbd1385dd7f43
Gianluca Peretti <gianlucaperetti95@gmail.com>
2021-01-18 21:20:00 -0300

    Add files via upload
    Add all files inside "serverless" folder

---
+++ serverless/api/routes/auth.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const crypto = require("crypto")
+const Users = require("../models/users")
+
+const {isAuthenticated} = require("../auth")
+
+const jwt = require("jsonwebtoken")
+const router = express.Router()
+
+// Función que encripta el id de usuario
+const signToken = (_id) => {
+    return jwt.sign({_id}, "mi-secreto", {
+        expiresIn: 60 * 60 * 24 * 365,
+    })
+}
+
+router.post("/register", (req, res) => {
+    const {email, password} = req.body
+    crypto.randomBytes(16, (err, salt) => {
+        const newSalt = salt.toString("base64")
+        crypto.pbkdf2(password, newSalt, 10000, 64, "sha1", (err, key) => {
+            const encryptedPassword = key.toString("base64")
+            Users.findOne({email}).exec()
+                .then(user => {
+                    if (user) {
+                        return res.send("El usuario ya existe")
+                    }
+                    Users.create({
+                        email, 
+                        password: encryptedPassword,
+                        salt: newSalt,
+                    }).then(() => {
+                        res.send("Usuario creado con éxito")
+                    })
+                })
+        })
+    })
+})
+
+router.post("/login", (req, res) => {
+    const {email, password} = req.body
+    Users.findOne({email}).exec()
+        .then(user => {
+            if (!user) {
+                return res.send("Usuario y/o contraseña incorrecta")
+            }
+            crypto.pbkdf2(password, user.salt, 10000, 64, "sha1", (err, key) => {
+                const encryptedPassword = key.toString("base64")
+                if (user.password === encryptedPassword) {
+                    const token = signToken(user._id)
+                    return res.send({token})
+                }
+                return res.send("Usuario y/o contraseña incorrecta")
+        })
+    })
+})
+
+router.get("/me", isAuthenticated, (req, res) => {
+    res.send(req.user)
+})
+
+module.exports = router
